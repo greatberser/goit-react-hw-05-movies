b956c9f15bba872ad59bb479cb622e72dfaacaeb
Maks <maks135254@gmail.com>
2024-01-13 18:57:33 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
-export const App = () => {
+
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    </>
   );
 };
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/';
+axios.defaults.params = {
+    api_key: '16a76550fab4a9d620c3cb364bb90d65',
+}
+
+export const getTrending = async () => {
+    const response = await axios.get(`3/trending/movie/day`);
+    return response.data.results;
+}
+
+export const getByQuery = async query => {
+    const response = await axios.get(`3/search/movie?query=${query}`);
+    return response.data.results;
+}
+
+export const getMovieDetails = async id => {
+    const response = await axios.get(`3/movie/${id}`);
+    return response.data;
+}
+
+export const getMovieCredits = async id => {
+    const response = await axios.get(`3/movie/${id}/credits`);
+    return response.data.cast;
+}
+
+export const getMovieReviews = async id => {
+    const resposnse = await axios.get(`3/movie/${id}/reviews`);
+    return resposnse.data.results;
+}
